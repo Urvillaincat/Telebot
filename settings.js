f73f1025ae5fc3e7dbc2fcba562e8b205168ce4f
Urvillaincat <lyzeen01@gmail.com>
2025-06-12 09:54:56 +0700

    Update settings.js
    Ok

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN" //create bot here https://t.me/BotFather and get the bot token
-global.BOT_NAME = "Alphabot-Mdོ" //your bot name
-global.OWNER_NAME = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //your name
+global.BOT_TOKEN = "7679022009:AAFzegaIS23Z1ZOvSYoqRJiV8_W0mreRwAs" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_NAME = "Essence_Solbot-Mdོ" //your bot name
+global.OWNER_NAME = "Essence_Solbot ×፝֟͜×" //your name
 global.OWNER_NUMBER = "62887435047326" //your telegram number
-global.OWNER = ["https://t.me/zeeoneofc", "https://t.me/zeeone_ofc"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER = ["https://t.me/essencesol", "https://t.me/essence_sol"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
